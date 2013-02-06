1de09d471cbbf0bd4c3a32b506768790346a9184
Takanotsume <michael.simonini@gmail.com>
2013-02-06 22:40:48 +0100

    Allow twitter filter select

--- twitter.js
+++ twitter.js
@@ -1,4 +1,5 @@
-var express = require('express')
+var config = require('./config')
+  , express = require('express')
   , app = express()
   , server = require('http').createServer(app)
   , io = require('socket.io').listen(server)
@@ -6,20 +7,26 @@ var express = require('express')
 
 server.listen(3000);
 
-// Go to https://dev.twitter.com/apps
 var twit = new twitter({
-  consumer_key: '',
-  consumer_secret: '',
-  access_token_key: '',
-  access_token_secret: ''
+  consumer_key: config.twitter.consumer_key,
+  consumer_secret: config.twitter.consumer_secret,
+  access_token_key: config.twitter.access_token_key,
+  access_token_secret: config.twitter.access_token_secret
 });
 
-twit.stream('statuses/filter', { track: ['#SRCC'] }, function(stream) {
+twit.stream('statuses/filter', { track: ['@RTBFinfo'] }, function(stream) {
   stream.on('data', function (data) {
     io.sockets.volatile.emit('tweet', {
       user: data.user.screen_name,
-      text: data.text
+      text: data.text,
+      profile_image_url: data.user.profile_image_url
     });
+  });
+});
+
+io.sockets.on('connection', function (socket) {
+  socket.on('track', function (data) {
+    console.log(data.track);
   });
 });
 
