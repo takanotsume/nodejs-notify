32bb66a58b3c781ccdcad25fe4d46421444563bf
Takanotsume <michael.simonini@gmail.com>
2013-02-06 21:14:17 +0100

    Add config file and deploy nodejitsu ignore

---
+++ config.sample.js
@@ -0,0 +1,11 @@
+var config = {}
+
+config.twitter = {};
+
+// Go to https://dev.twitter.com/apps
+config.twitter.consumer_key = '',
+config.twitter.consumer_secret = '',
+config.twitter.access_token_key = '',
+config.twitter.access_token_secret = ''
+
+module.exports = config;
